f58ebff2ea7830927e0abe9b2c2ae97cd7bfdd3a
snpadhycutm <snpadhy3091@gmail.com>
2025-07-14 19:17:47 +0530

    Update tb.v

--- test/tb.v
+++ test/tb.v
@@ -3,27 +3,30 @@
 
 module tb ();
 
-  reg clk;
-  reg rst_n;
-  reg ena;
-  reg [7:0] ui_in;
-  reg [7:0] uio_in;
-  wire [7:0] uo_out;
-  wire [7:0] uio_out;
-  wire [7:0] uio_oe;
+    reg clk;
+    reg rst;
+    reg ena;
 
+    reg [7:0] ui_in;
+    reg [7:0] uio_in;
 
-  // DUT instantiation
-  tt_um_turbo_enc_8bit dut (
-      .clk(clk),
-      .rst(~rst_n),          // Your module uses active-high reset
-      .ui_in(ui_in),
-      .uio_in(uio_in),
-      .uo_out(uo_out)
-      // No uio_out/uio_oe in your design, so not connected
-  );
+    wire [7:0] uo_out;
+    wire [7:0] uio_out;
+    wire [7:0] uio_oe;
 
- 
+    // Instantiate the DUT
+    tt_um_turbo_enc_8bit dut (
+        .clk(clk),
+        .rst(rst),
+        .ena(ena),
+        .ui_in(ui_in),
+        .uio_in(uio_in),
+        .uo_out(uo_out),
+        .uio_out(uio_out),
+        .uio_oe(uio_oe)
+    );
 
+   
 endmodule
+
 
